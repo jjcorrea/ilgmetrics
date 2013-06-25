99b56fb695fd860104dcd5d0358cfe7498344532
joelcorrea <joel@absoluta.net>
2013-06-25 15:00:17 -0300

    Disabled django admin

--- metrics/views.py
+++ metrics/views.py
@@ -46,6 +46,7 @@ def cfd_chart_page(request):
     en_in = ''
     tm_in = ''
     tp_in = ''
+    
     if request.method == 'POST':
         st_in = request.POST['start_datepick']
         en_in = request.POST['end_datepick']
