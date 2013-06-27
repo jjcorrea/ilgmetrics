2f27d8695e60188f211a6820702b59ffa2f61224
joelcorrea <joel@absoluta.net>
2013-06-27 09:38:00 -0300

    Added new django admin models

---
+++ metrics/admin.py
@@ -0,0 +1,8 @@
+from models import *
+from django.contrib import admin
+
+admin.site.register(TrelloBoard)
+admin.site.register(KanbanStatus)
+admin.site.register(KanbanStatusSynonym)
+admin.site.register(Team)
+admin.site.register(Sprint)
---
+++ metrics/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class TrelloBoard(models.Model):
+    team = models.ForeignKey('Team')
+    hash_id = models.CharField(max_length=30)
+    ignore_list = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return '%s - %s' % (self.team, self.hash_id)
+    
+class KanbanStatus(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+class KanbanStatusSynonym(models.Model):
+    kanban_status = models.ForeignKey('KanbanStatus')
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return '%s - %s' % (self.kanban_status, self.name)
+    
+class Team(models.Model):
+    team_name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.team_name
+    
+class Sprint(models.Model):
+    team = models.ForeignKey('Team')
+    sprint_name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return '%s - %s' % (team, sprint_name)
